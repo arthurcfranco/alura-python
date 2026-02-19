989346e927820e26c2e75d7c827126e555515030
arthurcfranco <arthurfranco1996@gmail.com>
2026-02-19 15:03:07 -0400

    exercicios alura

--- praticando projetos/validar-cpf.py
+++ praticando projetos/validar-cpf.py
@@ -1,9 +1,9 @@
 # Programa pra validar CPF.
 
 def validar_cpf(cpf):
-    if not cpf.isdigit():
+    if not cpf.isdigit(): #isdigit para garantir que não há letras ou caracteres.
         return "Erro: O CPF deve conter apenas números."
-    if len(cpf) != 11:
+    if len(cpf) != 11: #len != 11 - verificar quantidade de letras.
         return "Erro: O CPF deve ter exatamente 11 dígitos."
     return "CPF válido."
  
